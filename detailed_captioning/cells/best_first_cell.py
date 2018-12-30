2f6669a9109d5e9149fb702140bd6a2ea998c2af
Brandon Trabucco <brandon@btrabucco.com>
2018-12-30 10:18:40 +0000

    finished best first cell

--- detailed_captioning/cells/best_first_cell.py
+++ detailed_captioning/cells/best_first_cell.py
@@ -6,57 +6,69 @@ np preprint yet.'''
 import numpy as np
 import tensorflow as tf
 import collections
-from detailed_captioning.utils import tile_with_new_axis
 from detailed_captioning.cells.image_caption_cell import ImageCaptionCell
 
 
-def BestFirstInsert(running_ids, word_ids, pointer_ids, name=None):
+def BestFirstInsert(running_ids, lengths, word_ids, pointer_ids):
     
-    def best_first_function(running_ids, word_ids, pointer_ids):
+    def best_first_function(running_ids, lengths, word_ids, pointer_ids):
         """ Inserts into the running_ids list using the best_first approach.
         Args:
             running_ids: a tf.int32 Tensor has shape [batch_size, max_sequence_length]
+            lengths: a tf.int32 Tensor has shape [batch_size]
             word_ids: a tf.int32 Tensor has shape [batch_size]
             pointer_ids: a tf.int32 Tensor has shape [batch_size]
         Returns:
-            a tf.int32 Tensor has shape [batch_size, max_sequence_length] """
-        out_array = np.zeros(running_ids.shape, dtype=np.int32)
-        for i in range(word_ids.size):
-            (out_array[i, :pointer_ids[i] + 1], out_array[i, pointer_ids[i] + 1], 
-             out_array[i, pointer_ids[i] + 2:]) = (
-                 running_ids[i, :pointer_ids[i] + 1], word_ids[i], running_ids[i, pointer_ids[i] + 1:-1])
-        return out_array
+            running_ids: a tf.int32 Tensor has shape [batch_size, max_sequence_length] 
+            lengths: a tf.int32 Tensor has shape [batch_size] """
+        
+        out_running_ids = np.zeros(running_ids.shape, dtype=np.int32)
+        out_lengths = np.zeros(lengths.shape, dtype=np.int32)
+        for i in range(running_ids.shape[0]):
+            if pointer_ids[i] >= 0 and pointer_ids[i] < running_ids.shape[1]:
+                (out_running_ids[i, :pointer_ids[i] + 1], out_running_ids[i, pointer_ids[i] + 1], 
+                 out_running_ids[i, pointer_ids[i] + 2:], out_lengths[i]) = (
+                    running_ids[i, :pointer_ids[i] + 1], word_ids[i], 
+                    running_ids[i, pointer_ids[i] + 1:-1], lengths[i] + 1)
+            else:
+                out_running_ids[i, :], out_lengths[i] = running_ids[i, :], lengths[i]
+                
+        return out_running_ids, out_lengths
     
-    return tf.py_func(best_first_function, [running_ids, word_ids, 
-        pointer_ids], tf.int32, name=name)
+    return tf.py_func(best_first_function, [running_ids, lengths, word_ids, 
+        pointer_ids], tf.int32)
 
 
-_BestFirstStateTuple = collections.namedtuple("BestFirstStateTuple", ("running_ids", "length"))
+_BestFirstStateTuple = collections.namedtuple("BestFirstStateTuple", (
+    "running_ids", "lengths", "pointer_ids"))
 class BestFirstStateTuple(_UpDownStateTuple):
     __slots__ = ()
     @property
     def dtype(self):
-        (running_ids, length) = self
-        if not running_ids.dtype == length.dtype:
-            raise TypeError("Inconsistent internal state: %s vs %s" %
-                            (str(running_ids.dtype), str(length.dtype)))
-        return running_ids.dtype
+        (running_ids, lengths, pointer_ids) = self
+        if (not running_ids.dtype == tf.int32 or 
+                not lengths.dtype == tf.int32 or
+                not pointer_ids.dtype == tf.int32):
+            raise TypeError("Inconsistent internal state: %s vs %s vs %s" %
+                            (str(running_ids.dtype), 
+                             str(lengths.dtype), 
+                             str(pointer_ids.dtype)))
+        return tf.int32
 
     
 class BestFirstCell(ImageCaptionCell):
 
-    def __init__(self, image_caption_cell
+    def __init__(self, image_caption_cell, word_embeddings,
+                 maximum_sequence_length=1000,
                  name="best_first", initializer=None, **kwargs):
         super(BestFirstCell, self).__init__(**kwargs)
         self.image_caption_cell = image_caption_cell
-        def softmax_attention(x):
-            x = tf.transpose(x, [0, 2, 1])
-            x = tf.nn.softmax(x)
-            x = tf.transpose(x, [0, 2, 1])
-            return x
-        self.attention_layer = tf.layers.Dense(1, kernel_initializer=initializer, 
-            name=(name + "/attention_layer"), activation=softmax_attention)
-        self._state_size = BestFirstStateTuple(running_ids=tf.TensorShape([2]))
+        self.embeddings_map = tf.get_variable(name + "/embeddings_map", dtype=tf.float32,
+            initializer=tf.constant(word_embeddings, dtype=tf.float32))
+        self.pointer_layer = tf.layers.Dense(1, kernel_initializer=initializer, 
+            name=(name + "/pointer_layer"))
+        self._state_size = BestFirstStateTuple(running_ids=maximum_sequence_length, 
+            lengths=1, pointer_ids=1)
         self._output_size = self.image_caption_cell.output_size
 
     @property
@@ -68,13 +80,32 @@ class BestFirstCell(ImageCaptionCell):
         return self._output_size
 
     def __call__(self, inputs, state):
-        l_inputs = tf.concat([self.mean_image_features, inputs], 1)
-        l_outputs, l_next_state = self.language_lstm(l_inputs, state)
-        return l_outputs, l_next_state
+        
+        previous_running_ids = state.running_ids
+        previous_lengths = tf.squeeze(state.lengths)
+        previous_pointer_ids = tf.squeeze(state.pointer_ids)
+        previous_word_ids = inputs
+        current_running_ids, current_lengths = BestFirstInsert(previous_running_ids, 
+            previous_lengths, previous_word_ids, previous_pointer_ids)
+        batch_size = tf.shape(current_running_ids)[0]
+        initial_state = self.image_caption_cell.zero_state(batch_size, tf.float32)
+        activations, _state = tf.nn.dynamic_rnn(self.image_caption_cell, 
+            tf.nn.embedding_lookup(self.embeddings_map, current_running_ids),
+            sequence_length=current_lengths, initial_state=initial_state)
+        # TODO: Need to mask out the out of bounds logits based on length
+        pointer_logits = tf.squeeze(self.pointer_layer(activations))
+        # TODO: This will need to change and use the ground truth
+        current_pointer_ids = tf.argmax(pointer_logits, axis=-1, output_type=tf.int32)
+        next_state = BestFirstStateTuple(running_ids=current_running_ids, 
+            lengths=tf.expand_dims(current_lengths, 1), 
+            pointer_ids=tf.expand_dims(current_pointer_ids, 1))
+        pointed_activations = tf.gather(activations, current_pointer_ids, axis=1)
+        return pointed_activations, next_state
     
     @property
     def trainable_variables(self):
-        cell_variables = self.language_lstm.trainable_variables 
+        cell_variables = (self.language_lstm.trainable_variables + 
+            self.pointer_layer.trainable_variables + [self.embeddings_map])
         return cell_variables
     
     @property
@@ -83,7 +114,8 @@ class BestFirstCell(ImageCaptionCell):
     
     @property
     def variables(self):
-        cell_variables = self.language_lstm.variables 
+        cell_variables = (self.language_lstm.variables + 
+            self.pointer_layer.variables + [self.embeddings_map])
         return cell_variables
     
     @property
